7439f9615262280f34435a8e0f3b4bd8817a494d
samirmuwal <samirkjan2627@gmail.com>
2026-06-15 23:15:43 +0530

    change Reservation

--- src/app/page.js
+++ src/app/page.js
@@ -27,7 +27,7 @@ export default async function Home() {
   return (
     <main className="bg-gray-50 min-h-screen">
       <section className="w-full px-6 md:px-10 lg:px-14 py-16">
-        <div className="bg-black text-white rounded-3xl p-8 md:p-14 grid md:grid-cols-2 gap-10 items-center">
+        <div className="bg-black text-white rounded-3xl p-8 md:p-14 px-4 md:px-10 gap-10 items-center">
           <div>
             <p className="text-yellow-400 font-semibold mb-3">
               Trusted Electrical Store
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -137,7 +137,7 @@ export default function ProductsPage() {
       {filtered.length === 0 ? (
         <p>No products found.</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 flex-col md:flex-row gap-6">
           {filtered.map((item) => (
             <Link
               href={`/products/${item._id}`}
